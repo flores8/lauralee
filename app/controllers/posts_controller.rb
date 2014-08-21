82d7729751993d364b465df6795e15082b7ee120
flores8 <flores.lauralee@gmail.com>
2014-08-20 20:41:05 -0700

    added option to edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,5 +23,17 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else 
+      flash[:errror] = "There was a problem updating your post. Please try again."
+      render :edit
+    end
   end
 end
