2bf33de06a0a941fd710be1b5345953a86bae539
flores8 <flores.lauralee@gmail.com>
2014-08-20 21:15:42 -0700

    created association and updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
 require 'faker'
 
+# Create Users
+5.times do
+	user = User.new(
+		name: Faker::Name.name,
+		email: Faker::Internet.email,
+		password: Faker::Lorem.characters(10)
+	)
+	user.skip_confirmation!
+	user.save!
+end
+users = User.all
+
 # Create Posts
 50.times do
 	Post.create(
-		title: Faker::Lorem.sentence,
+		user: users.sample,
+		title: Faker::Lorem.sentence(3),
 		body: Faker::Lorem.paragraph
 		)
 end
@@ -17,6 +30,12 @@ posts = Post.all
 		)
 end
 
+User.first.update_attributes!(
+	email: 'lauralee@majorfinder.com',
+	password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
