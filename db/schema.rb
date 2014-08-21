ac8e93909122dd777e61dfc4a78dee9efa6fc992
flores8 <flores.lauralee@gmail.com>
2014-08-20 21:46:56 -0700

    added role to user and required authorized user to edit, new post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821035936) do
+ActiveRecord::Schema.define(version: 20140821044437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140821035936) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
