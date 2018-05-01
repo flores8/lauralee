c54644e03013e8476b378b6685a9536633b7dc6c
Lauralee Flores <flores.lauralee@gmail.com>
2018-04-30 21:48:09 -0700

    force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Lauralee::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -77,5 +77,5 @@ Lauralee::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'lauralee.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.lauraleeflores.com.herokudns.com' }
 end
